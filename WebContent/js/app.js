736fb193f9cc699e6c57f3ebb640b6f8f5c05013
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-02-10 21:24:04 +0200

    Prepared geristration fields

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -5,26 +5,46 @@
 (function() {
 	var app = angular.module('library', []);
 	var errorUsername = false;
-	app.controller('LibController', function() {
+	app.controller('LibController',
+
+	function() {
 		this.products = books;
 
 	});
-
 	app
 			.controller(
 					'LandingController',
 					function() {
 						/* Properties */
-						/* don't forget to change these two back */
-						this.userLogin = false;
-						this.userRegister = true;
+						// TODO: don't forget to change these two back
+						this.userLogin = true;
+						this.userRegister = false;
 						this.errorUsername = false;
 						this.error1 = "";
 						this.error2 = "";
 						this.authFailureWarningHidden = true;
+						this.newUserNameOk = true;
+						this.newEmailOk = true;
+						this.newAdressOk = true;
+						this.newBlockNumberOk = true;
+						this.newCityOk = true;
+						this.newZIPOk = true;
+						this.newPhoneOk = true;
+						this.newPassOk = true;
+						this.newNickOk = true;
+						this.newPhotoOk = true;
+
 						this.usernameError = "Username must be unique, can have only English characters and can't be longer than 10 characters";
 						this.emailerror = "Please provide a valid email, that has @ character and a domain dot";
-						this.adressError = "AEnglish characters only, longer than 3 characters";
+						this.adressError = "Address must contain English characters only, and be longer than 3 characters";
+						this.blockError = "Block number has to be a number";
+						this.cityError = "City name must contain English characters only, and be longer than 3 characters";
+						this.zipError = "ZIP has to be exactly seven numbers";
+						this.phoneError = "Phone number must be valid 0545-333-22-11 or 046-222-210"
+						this.passError = "Password can be at most 8 characters";
+						this.nickError = "Nickname must be unique and up to 20 characters";
+						this.photoError = "Not recognized as valid URL";
+							
 
 						/* Functions */
 
@@ -39,13 +59,15 @@
 						/** Register new user */
 						this.register = function() {
 							/* Input check */
-
+							var passed = true;
+							/* Test Username */
+							if (!testNewUsername())
+								passed = false;
 							/* what happens if there is an error */
 							this.authFailureWarningHidden = false;
 							this.error1 = "";
 							this.error2 = "";
 						};
-						this.register();
 					});
 
 	var books = [
@@ -56,12 +78,32 @@
 		description : 'Some description',
 		canRead : true,
 		canBuy : false,
+		reviews : [ {
+			author : "user3",
+			body : "Some",
+		}, {
+			author : "user4",
+			body : "Review",
+		} ]
 	}, {
 		name : '12 rules to meaningfull life',
 		price : 17.1,
 		description : 'Some description',
 		canRead : true,
 		canBuy : true,
+		imageURL : "",
+		reviews : [ {
+			author : "user1",
+			body : "Some review",
+		}, {
+			author : "user2",
+			body : "Some review",
+		} ]
 	} ];
+
+	function testNewUsername() {
+
+	}
+	;
 
 })();
