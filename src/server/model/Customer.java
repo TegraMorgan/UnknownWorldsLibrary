f64c9ce692c0f83258966f4863d8279536adffb5
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-02-14 18:17:17 +0200

    Added getters and setters for Customer.java
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- src/server/model/Customer.java
+++ src/server/model/Customer.java
@@ -38,4 +38,68 @@ public Customer(ResultSet rs) throws SQLException {
 	this.photo_url=rs.getString("photo_url");
 }
 
+public int getUid() {
+	return uid;
+}
+
+public void setUid(int uid) {
+	this.uid = uid;
+}
+
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getPhone() {
+	return phone;
+}
+
+public void setPhone(String phone) {
+	this.phone = phone;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getNickname() {
+	return nickname;
+}
+
+public void setNickname(String nickname) {
+	this.nickname = nickname;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public String getPhoto_url() {
+	return photo_url;
+}
+
+public void setPhoto_url(String photo_url) {
+	this.photo_url = photo_url;
+}
+
 }
