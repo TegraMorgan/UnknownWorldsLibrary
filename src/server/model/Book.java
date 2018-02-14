43a68650371bd1aa1c3680a723d0019f8f902e52
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-02-15 00:12:08 +0200

    Added book model
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

---
+++ src/server/model/Book.java
@@ -0,0 +1,44 @@
+package server.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Book {
+	private int bid;
+	private String name;
+	private String author;
+	private String genre;
+	private String imageUrl;
+	private Double price;
+	private String description;
+	private String filepath;
+
+	/*
+	 * bid int NOT NULL, name varchar(255) DEFAULT NULL, author varchar(45) DEFAULT
+	 * NULL, genre varchar(45) DEFAULT NULL, image_url varchar(255) DEFAULT NULL,
+	 * price double DEFAULT NULL, description varchar(500) DEFAULT NULL, filepath
+	 * varchar(500) DEFAULT NULL, PRIMARY KEY (bid)
+	 */
+	public Book(int bId, String nAme, String aUthor, String gEnre, String iMageUrl, Double pRice, String dEscription,
+			String fIlepath) {
+		this.bid = bId;
+		this.name = nAme;
+		this.author = aUthor;
+		this.genre = gEnre;
+		this.imageUrl = iMageUrl;
+		this.price = pRice;
+		this.description = dEscription;
+		this.filepath = fIlepath;
+	}
+	
+	public Book(ResultSet rs) throws SQLException {
+		this.bid=rs.getInt("bid");
+		this.name=rs.getString("name");
+		this.author=rs.getString("author");
+		this.genre=rs.getString("genre");
+		this.imageUrl=rs.getString("image_url");
+		this.price=rs.getDouble("price");
+		this.description=rs.getString("description");
+		this.filepath=rs.getString("filepath");
+	}
+}
