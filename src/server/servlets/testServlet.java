fcc620dc11b2d8e60460d6c053efcaf8086a3d6e
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-02-15 17:48:11 +0200

    Saving progress of test Servlet
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- src/server/servlets/testServlet.java
+++ src/server/servlets/testServlet.java
@@ -1,11 +1,14 @@
 package server.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -42,8 +45,23 @@ public class testServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		System.out.println("testServlet started");
 		Gson gson = new GsonBuilder().setDateFormat("MMM dd,yyyy HH:mm:ss").create();
-		Customer customer = gson.fromJson(request.getReader(), Customer.class);
-		
+		Customer cc = gson.fromJson(request.getReader(), Customer.class);
+		System.out.printf(" Nickname = %s, Username = %s",cc.getNickname(),cc.getUsername());
+		response.setContentType("application/json);charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		String data;
+		try {
+			HttpSession session = request.getSession();
+			request.setAttribute("customer", cc);
+			session.setAttribute("customer", cc); 
+			request.setAttribute("httpSession", session);
+			String CustomerInJson = gson.toJson(cc, Customer.class);
+			data = "{\"result\": \"success\",\"user\": " + CustomerInJson + "}";
+			pw.println(data);
+			pw.close();
+		} catch (Exception e) {
+
+		}
 	}
 
 }
