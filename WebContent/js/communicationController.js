ca202b17a5112a6111604117a20d394e2a5dc571
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-02-15 18:28:57 +0200

    template for developing client server communication
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- WebContent/js/communicationController.js
+++ WebContent/js/communicationController.js
@@ -25,16 +25,29 @@ angular.module('helper',[])
    */
 angular.module('myApp', [ 'helper' ])
 .controller('myController', [ '$scope', '$http', 'HELPER', function($scope, $http, HELPER) {
+  //GET REQUEST
   $http.get("http://localhost:8080/ExampleServletv3/customers").success(function(response) {
     $scope.result = response;
   })
-  $http.get("wrongfilename.htm").then(function(response) {
-    // First function handles success
-    $scope.content = response.data;
-  }, function(response) {
-    // Second function handles error
-    $scope.content = "Something went wrong";
-  });
+  //POST REQUEST
+  var values = $("#someform").serialize();
+  $('#someform :input').prop("disabled",true);
+  
+   $.ajax({
+       type: "POST",
+       url: '/APP_NAME/SERVLETNAME',
+       data: values,
+     }).done(function(data,textStatus, jqXHR) {
+          alert( "just for debuggin - success" );
+         console.log("name= " + data.name + "; email="  +data.email + "; textStatus= " + textStatus );
+         $('#notification_success').show();
+     }).always(function(data,textStatus, jqXHR) {
+         alert("just for debuggin -  in always");
+     }).fail(function(data,textStatus, jqXHR) {
+          alert( "just for debuggin -  failure" );
+          $('#notification_failure').show();
+          $('#registerSubmit :input').prop("disabled",false);
+     });
 
 } ]);
 
