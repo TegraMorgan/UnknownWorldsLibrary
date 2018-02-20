65de5132e3d95fd5fed89cb37a5b64938ad07b62
Tegra Morgan <tegra.morgan.net@gmail.com>
2018-02-20 02:50:49 +0200

    Improved haresponse handler
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- src/server/resopnse/LogInResponse.java
+++ src/server/resopnse/LogInResponse.java
@@ -1,19 +1,46 @@
 package server.resopnse;
 
-public class LogInResponse extends BasicResponse {
+import java.lang.reflect.Type;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import server.model.Customer;
+
+public class LogInResponse extends BasicResponse {
+	private Customer customer;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 	public LogInResponse() {
-		// TODO Auto-generated constructor stub
+		this.customer = null;
 	}
 
-	public LogInResponse(String su) {
+	public LogInResponse(String su, Customer cust) {
 		super(su);
-		// TODO Auto-generated constructor stub
+		this.customer = cust;
+	}
+
+	public java.lang.reflect.Type getType() {
+		return new TypeToken<LogInResponse>() {
+		}.getType();
+	}
+
+	public String tojson() {
+		Gson gson = new GsonBuilder().create();
+		Type type = this.getType();
+		return gson.toJson(this, type);
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 
 }
--- src/server/servlets/LogInServlet.java
+++ src/server/servlets/LogInServlet.java
@@ -16,6 +16,7 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import server.model.*;
+import server.resopnse.LogInResponse;
 
 /**
  * Servlet implementation class LogInServlet
@@ -49,23 +50,20 @@ public class LogInServlet extends HttpServlet {
 		response.setContentType("application/json; charset=UTF-8");
 		PrintWriter printWriter = response.getWriter();
 		String data;
-		try {
-			if (customer.getCustomer(customer.getUsername(), customer.getPassword()) > 0) {// this method connect to the
-																							// db and check the user
-																							// data return 1 if user is
-																							// exist else -1
-				customer.getMyLikes();
-				customer.getMyreviews();
-				customer.getMyBooks();
-				
+
+		try {// this method connect to the db and check the user data return 1 if user is
+				// exist else -1
+			LogInResponse resp = new LogInResponse();
+			if (customer.getCustomer(customer.getUsername(), customer.getPassword()) > 0) {
 				HttpSession session = request.getSession();
 				session.setAttribute("customer", customer);
 				// request.setAttribute("httpSession", session);
-				String CustomerInJson = gson.toJson(customer, Customer.class);
-				data = "{\"result\": \"success\",\"customer\": " + CustomerInJson + "}";
-				System.out.println("getting user 1>>>>>>>>>" + data);//////////
+				resp.setCustomer(customer);
+				resp.setResult("success");
+				data = resp.tojson();
 			} else {
-				data = "{ \"result\": \"fail\"}";
+				resp.setResult("fail");
+				data = resp.tojson();
 			}
 			printWriter.println(data);
 			printWriter.close();
